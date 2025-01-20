78465c2c96327c5cc3f05738b502d60c407a0b7f
Mirko Quaglia <gladiusservices@protonmail.com>
2025-01-20 19:21:35 +0100

    feat: setup eslint and prettier (#4)
    * feat: add eslint eslint-config-prettier eslint-plugin-prettier and prettier
    
    * feat: create .eslintrc.json
    
    * feat: create .prettierrc.json
    
    * fix: missing @react-native/eslint-config to extend
    
    * fix: missing typescript devDependency
    
    * feat: setup lint script
    
    * chore: run yarn install
    
    * fix(lint): all src files

--- src/style.ts
+++ src/style.ts
@@ -1,12 +1,12 @@
 import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
-    container: {
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-    itemContainer: {
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-  });
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  itemContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
