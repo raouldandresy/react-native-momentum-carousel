581bbd4686b29dab17afade7801f362ef1739a2b
Mirko Quaglia <gladiusservices@protonmail.com>
2025-01-20 19:39:42 +0100

    feat: improve example app (#10)
    * feat: showcase autoplay and loop
    
    * feat: showcase autoplayInterval

--- example/App.tsx
+++ example/App.tsx
@@ -5,23 +5,24 @@
  * @format
  */
 
-import React, { useRef, useState } from 'react';
+import React, {useCallback, useRef, useState} from 'react';
 import {
+  Button,
   Dimensions,
-  FlatList,
   Image,
   Pressable,
   SafeAreaView,
   ScrollView,
   StatusBar,
   StyleSheet,
+  Text, TextInput, TextInputProps,
   useColorScheme,
   View,
 } from 'react-native';
 import { CarouselMomentum } from 'react-native-momentum-carousel';
 
 import {
-  Colors
+  Colors,
 } from 'react-native/Libraries/NewAppScreen';
 
 const windowWidth = Dimensions.get('window').width;
@@ -33,29 +34,51 @@ function App(): React.JSX.Element {
   };
 
   const [index, setIndex] = useState(0);
+
+  const [autoplay, setAutoplay] = useState(false);
+  const [loop, setLoop] = useState(false);
+  const [autoplayInterval, setAutoplayInterval] = useState(3);
+
   const images = [
     { id: '1', source: 'https://picsum.photos/411/250/' },
     { id: '2', source: 'https://picsum.photos/411/250/' },
     { id: '3', source: 'https://picsum.photos/411/250/' },
     { id: '4', source: 'https://picsum.photos/411/250/' },
-    { id: '5', source: 'https://picsum.photos/411/250/' }
+    { id: '5', source: 'https://picsum.photos/411/250/' },
   ];
+
+  const handleToggleAutoplayPress = useCallback(() => {
+    setAutoplay((current) => !current);
+  }, []);
+
+  const handleToggleLoopPress = useCallback(() => {
+    setLoop((current) => !current);
+  }, []);
+
+  const handleAutoplayIntervalChange = useCallback<NonNullable<TextInputProps['onChangeText']>>((value) => {
+    const possibleValue = Number(value);
+    if (Number.isNaN(possibleValue)) {
+      return;
+    }
+
+    setAutoplayInterval(possibleValue ?? 1);
+  }, []);
 
   const renderItem = ({ item, index }) => {
     return (
     <Pressable style={styles.itemContainer} onPress={() => goToIndex(index)}>
       <Image source={{ uri: item.source }} style={styles.image}/>
     </Pressable>
-    )
+    );
   };
-  const flatListRef = useRef(CarouselMomentum); 
+  const flatListRef = useRef(CarouselMomentum);
 
   const goToIndex = (index) => {
-    console.log(flatListRef?.current?.getCurrentIndex())
+    console.log(flatListRef?.current?.getCurrentIndex());
     flatListRef?.current?.goToIndex(index);
-  }
+  };
   const onSnap = (index) => {
-    setIndex(index)}
+    setIndex(index);};
 
   return (
     <SafeAreaView style={backgroundStyle}>
@@ -63,6 +86,12 @@ function App(): React.JSX.Element {
         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
         backgroundColor={backgroundStyle.backgroundColor}
       />
+
+      <View style={styles.header}>
+        <Text>{`Autoplay: ${autoplay ? 'true' : 'false'}`}</Text>
+        <Text>{`Looping: ${loop ? 'true' : 'false'}`}</Text>
+      </View>
+
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
@@ -70,16 +99,27 @@ function App(): React.JSX.Element {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <CarouselMomentum 
-          ref={flatListRef}
-          data={images}
-          sliderWidth={windowWidth} 
-          itemWidth={windowWidth*0.8} 
-          renderItem={renderItem} 
-          onSnap={onSnap}
-          keyExtractor={(item)=> item.id}
-          inactiveScale={0.8}
+          <CarouselMomentum
+            ref={flatListRef}
+            data={images}
+            sliderWidth={windowWidth}
+            itemWidth={windowWidth * 0.8}
+            autoPlay={autoplay}
+            loop={loop}
+            autoPlayInterval={autoplayInterval * 1000}
+            renderItem={renderItem}
+            onSnap={onSnap}
+            keyExtractor={(item)=> item.id}
+            inactiveScale={0.8}
           />
+        </View>
+        <View style={styles.buttonsContainer}>
+          <Button title={'Toggle autoplay'} onPress={handleToggleAutoplayPress} />
+          <View style={styles.separator} />
+          <Button title={'Toggle loop'} onPress={handleToggleLoopPress} />
+          <View style={styles.separator} />
+          <Text>Set an autoplay interval in seconds</Text>
+          <TextInput style={styles.autoplayIntervalInput} value={String(autoplayInterval)} placeholder={'Set autoplay interval'} onChangeText={handleAutoplayIntervalChange} />
         </View>
       </ScrollView>
     </SafeAreaView>
@@ -87,6 +127,10 @@ function App(): React.JSX.Element {
 }
 
 const styles = StyleSheet.create({
+  header: {
+    marginBottom: 20,
+    alignItems: 'center',
+  },
   sectionContainer: {
     marginTop: 32,
     paddingHorizontal: 24,
@@ -105,13 +149,24 @@ const styles = StyleSheet.create({
   },
   itemContainer: {
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
   },
   image: {
     width: windowWidth * 0.8,
     height: 250,  // Altezza delle immagini
-    flex: 1
-  }
+  },
+  buttonsContainer: {
+    marginTop: 20,
+    alignItems: 'center',
+  },
+  separator: {
+    marginBottom: 10,
+  },
+  autoplayIntervalInput: {
+    width: 200,
+    padding: 10,
+    borderBottomWidth: 1,
+  },
 });
 
 export default App;
