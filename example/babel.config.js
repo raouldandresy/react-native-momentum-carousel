0564cdb797a174e56176075271bc3fc8428eaad1
gladius <gladiusservices@protonmail.com>
2025-01-22 19:53:23 +0100

    chore: comment babel.config.js

--- example/babel.config.js
+++ example/babel.config.js
@@ -4,6 +4,9 @@ const pak = require('../package.json');
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
   plugins: [
+    // We use this plugin to resolve the path to the source code of the library
+    // This is necessary because we are using a monorepo setup, and we don't
+    // want to directly install the library from npm.
     [
       'module-resolver',
       {
